9a54e2e634f0ddf7a75a965768f12e3ee6c345e7
Rafsan Jany <rafsanjany1213@gmail.com>
2022-11-04 18:10:48 +0600

    Update addition_1.asm

--- MIPS PROG /addition_1.asm
+++ MIPS PROG /addition_1.asm
@@ -1,14 +1,8 @@
-# File: Program3-1.asm
-# Author: Charles Kann
-# Purpose: To illustrate some addition operators
 # illustrate R format add operator
-li $t1, 100
-li $t2, 50
+li $t1, 100		#li stands for Load Immediate and is a convenient way of loading an immediate up to 32 bits in size.
+li $t2, 50		# in easy word loding a  constant valu
 add $t0, $t1, $t2
-# illustrate add with an immediate. Note that
-# an add with a pseudo instruction translated
-# into an addi instruction
+
+# illustrate add with an immediate. 
 addi $t0, $t0, 50
 add $t0, $t0, 50
-# using an unsign number. Note that the
-# result is not what is e
